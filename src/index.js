780be52f7c23541cb734f49536aceb8170b80ff2
Phani K <phanik@Phanis-MacBook-Air.local>
2023-12-26 21:30:02 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const page = (
+    <div>
+    <h1>My awesome website in React</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's a hireable skill</li>
+        <li>It's actively maintained by skilled people</li>
+    </ol>
+</div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(page)
