4e95d368443c13b6dc898622bef97ed799124070
Phani K <phanik@Phanis-MacBook-Air.local>
2024-01-04 16:58:38 -0800

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 
 const page = (
     <div>
-    <h1>My awesome website in React</h1>
-    <h3>Reasons I love React</h3>
-    <ol>
-        <li>It's composable</li>
+        <p>
+        <svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg" className="logo"><circle cx="0" cy="0" r="2" fill="currentColor"></circle><g stroke="currentColor" stroke-width="1" fill="none"><ellipse rx="10" ry="4.5"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse></g></svg>
+        </p>
+    <h1>Fun facts about React</h1>
+ 
+    <ul>
+        <li>was first released in 2013</li>
         <li>It's declarative</li>
         <li>It's a hireable skill</li>
         <li>It's actively maintained by skilled people</li>
-    </ol>
+    </ul>
 </div>
 )
 
