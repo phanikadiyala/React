d1e1dd996c8cde0cf2ad2cba74427bbd6bea43d7
Phani K <phanik@Phanis-MacBook-Air.local>
2024-01-04 19:44:35 -0800

    components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 import './index.scss';
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
-const page = (
-    <div>
-        <p>
-        <svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg" className="logo"><circle cx="0" cy="0" r="2" fill="currentColor"></circle><g stroke="currentColor" stroke-width="1" fill="none"><ellipse rx="10" ry="4.5"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse></g></svg>
-        </p>
-    <h1>Fun facts about React</h1>
+function  Page() {
+    return (
+        <div>
+             <Header />
+            <Main />
+           <Footer />
+        </div>
  
-    <ul>
-        <li>was first released in 2013</li>
-        <li>It's declarative</li>
-        <li>It's a hireable skill</li>
-        <li>It's actively maintained by skilled people</li>
-    </ul>
-</div>
-)
+    )
+
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(page)
+root.render(<Page />);
